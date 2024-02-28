ff2e262ca4bc4b0a3165bec5850f34cdceffeb9a
Zoltan <arsendulyan@yandex.ru>
2024-02-28 23:16:07 +0300

    Add files via upload

---
+++ HW1/HW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by Арсен Дулян on 28.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
